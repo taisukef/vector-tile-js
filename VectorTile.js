bfe44d098925d9330046f66e805220b3dff18383
Taisuke Fukuno <fukuno@jig.jp>
2021-05-07 07:24:17 +0900

    add es module

---
+++ VectorTile.js
@@ -0,0 +1,14 @@
+import { VectorTileLayer } from "./vectortilelayer.js";
+
+function VectorTile(pbf, end) {
+    this.layers = pbf.readFields(readTile, {}, end);
+}
+
+function readTile(tag, layers, pbf) {
+    if (tag === 3) {
+        var layer = new VectorTileLayer(pbf, pbf.readVarint() + pbf.pos);
+        if (layer.length) layers[layer.name] = layer;
+    }
+}
+
+export { VectorTile };
